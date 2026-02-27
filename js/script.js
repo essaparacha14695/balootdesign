511f954a86e32d6b7d0d714520d3de1cd3dc02ec
essaparacha14695 <essa.paracha@gmail.com>
2026-02-27 23:08:20 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Baloot Design Loaded");
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 4000);
